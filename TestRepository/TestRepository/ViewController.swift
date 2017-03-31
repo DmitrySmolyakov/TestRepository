a08bbd111bad6f3e39ee39ac3341bf2bf20d356a
Dmitry Smolyakov <dm.smolyakov@gmail.com>
2017-03-31 20:55:11 +0300

    initial commit

---
+++ TestRepository/TestRepository/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestRepository
+//
+//  Created by Dmitry Smolyakov on 3/31/17.
+//  Copyright © 2017 Dmitry Smolyakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
